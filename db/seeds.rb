7a8186a27a63b618a5ea864c54141258747a1137
Donald Ali <donaldali@yahoo.com>
2015-04-09 15:55:55 -0700

    Seed Database
    Create seed data for database with help of constants declaring the
    number of various sizes of lockers.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,30 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Customer.destroy_all
+Locker.destroy_all
+
+num_customers = 20
+
+# Create customers
+num_customers.times do
+  Customer.create( first_name: Faker::Name.first_name, 
+                   last_name:  Faker::Name.last_name,
+                   room_number: "#{ Faker::Number.number(4) }",
+                   checked_in: true )
+end
+Customer.create( first_name: "Foo", last_name:  "Bar",
+                 room_number: "4242", checked_in: true )
+
+# Create lockers
+def create_lockers(num, size)
+  num.times do |index|
+    number = "#{ size[0] }-#{ index + 1 }"
+    Locker.create( number: number, size: size, assigned: false )
+  end
+end
+
+create_lockers(NUM_SM_LOCKERS, "SMALL")
+create_lockers(NUM_MD_LOCKERS, "MEDIUM")
+create_lockers(NUM_LG_LOCKERS, "LARGE")
